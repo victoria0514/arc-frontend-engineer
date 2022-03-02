0ef3ac8d37dc8fc89add80bc97a57b6fc8e4765c
huilin <huilin.zou@cix.csi.cuny.edu>
2022-03-02 12:40:43 -0500

    FEAT-academic btn

---
+++ arc/src/Academics/AcademicsBtn.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './AcademicsBtn.css'
+
+function AcademicsBtn(){
+
+  return (
+    <div>
+        <button
+        className='academics'>
+            <a className='title'>Academics</a>
+            <br/>
+            <a className='description'>Ea repellendus natus sed dolorem quam sed quia recusandae ut nemo voluptatem et be </a>
+        </button>
+    </div>
+  )
+}
+export default AcademicsBtn;
---
+++ arc/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AcademicsBtn from './Academics/AcademicsBtn';
+
+function App() {
+  return (
+    <div className="App">
+      <AcademicsBtn/>
+    </div>
+  );
+}
+
+export default App;
